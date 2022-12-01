988e38adc59f9b0c054f402dc714dd9f3191034d
Oleksii Bolshunov <bolshunov_oleksii@ukr.net>
2022-11-30 22:23:16 -0500

    update trainers, add media, update form validation

--- form_validation.js
+++ form_validation.js
@@ -1,4 +1,9 @@
 let t;
+const regexEmail = '[a-z0-9]+@[a-z]+\.[a-z]{2,3}';
+const regexPhone = '^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$';
+
+const email = new RegExp(regexEmail);
+const phone = new RegExp(regexPhone);
 
 let showMessage = (message) => {
     if(t) clearTimeout(t);
@@ -23,9 +28,18 @@ let reset = () => {
 
 }
 
+let checkEmail = (value) => {
+    return email.test(value);
+}
+
+let checkPhone = (value) => {
+    return phone.test(value);
+}
+
 let getValues = () =>{
-    let message = "Please enter all fields. You are missing: "
-    let show = false;
+    let message = "Missing fields: ";
+    let message2 = "Incorrect values: ";
+    let show = false, secondMessage = false;
     const elements = [
         document.getElementById("fname"),
         document.getElementById("lname"),
@@ -39,12 +53,20 @@ let getValues = () =>{
         if(elements[i].value == ''){
             show = true;
             message += fieldNames[i] + ", ";
+        } else if (i == 2 && !checkEmail(elements[i].value) || i == 3 && !checkPhone(elements[i].value)) {
+            message2 += fieldNames[i] + ", ";
+            secondMessage = true
         }
     }
-    
 
     if(show) {
         message = message.slice(0, message.length - 2) + ".";
+        if(secondMessage) {
+            message2 = message2.slice(0, message2.length - 2) + ".";
+            message += " " + message2;
+        }
+    } else if(secondMessage){
+        message = message2.slice(0, message2.length - 2) + ".";
     } else {
         reset();
         message = "Your registration form has been submitted";
--- scroller.js
+++ scroller.js
@@ -12,13 +12,9 @@ class Scroller {
         let leftButton = document.getElementById(this.leftButtonId);
         let rightButton = document.getElementById(this.rightButtonId);
 
-        // if(this.count == 0) leftButton.setAttribute('disabled', '');
-        // else if(this.count == 1) leftButton.removeAttribute('disabled');
         if(this.count == 0) leftButton.style.display = "none";
         else if(this.count == 1) leftButton.style.display = "block";
 
-        // if(this.count == this.length - 1) rightButton.setAttribute('disabled', '');
-        // else if(this.count == this.length - 2) rightButton.removeAttribute('disabled');
         if(this.count == this.length - 1) rightButton.style.display = "none";
         else if(this.count == this.length - 2) rightButton.style.display = "block";
     }
